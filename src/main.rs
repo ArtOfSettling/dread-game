8c412c649eb7201d73f818ca693e0d2a518f939d
Terry Magnus Drever <github.snowbird189@passmail.net>
2024-08-07 18:54:49 -0700

    Add tracing and subscriber libraries. Validate subscriber pattern works and update the client to use our new builder pattern.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+use std::env;
 use dread_engine::application::{Application, Definition};
+use dread_engine::core::CommandLineArgs;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 fn main() {
-    Application {
-        application_definition: Definition {
-            // The title that you would like to use for your application.
-            title: "Dread Game".to_string(),
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
 
-            // For now, there is no working directory usage, but you will ultimately be able to specify
-            working_directory: "".to_string()
-        }
-    }.run();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    let application_definition =
+        // start with default, sensible values
+        Definition::default()
+        // only specifying a title
+        .with_title("Dread Game".to_string())
+        // overriding any defaults with data from the command line
+        .override_with(CommandLineArgs::build(&env::args().collect()));
+
+    Application::new(application_definition)
+        .run();
 }
